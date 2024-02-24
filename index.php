fe6796b6a3be31685347bfa94526df0dc0676b5d
Brandon <brandonde2002@gmail.com>
2024-02-24 14:12:52 -0600

    trabajo hecho222

--- index.php
+++ index.php
@@ -3,39 +3,72 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Título de tu página</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <style>
+        /* Agrega estilos personalizados aquí */
+        .feature {
+            margin-bottom: 20px;
+        }
+        .feature img {
+            max-width: 100%;
+            height: auto;
+        }
+        .login-form {
+            max-width: 400px;
+            margin: 0 auto;
+        }
+    </style>
 </head>
 <body>
-    <?php require_once"public/navBar.php";?>
+    <?php require_once "public/navBar.php"; ?>
 
-    <div class="row text-center pt-4">
-        <div class="col">
+    <div class="container py-5">
 
-            <h3>Por que elegir con nosotros?</h3>
-            <div class="row">
-                <div class="col img-fluid"><img src="img/estadisticas.png" alt=""></div>
-                <div class="col img-fluid"><img src="img/equipo (4).png" alt=""></div>
+    <div class="row text-center">
+            <div class="col">
+                <h2>Inicia sesión</h2>
+                <form class="login-form">
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Correo electrónico</label>
+                        <input type="email" class="form-control" id="email" placeholder="Correo electrónico">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Contraseña</label>
+                        <input type="password" class="form-control" id="password" placeholder="Contraseña">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Iniciar sesión</button>
+                </form>
+                <br>
+                Ya tienes una cuenta? <a href="">Login aqui</a>
             </div>
-
         </div>
 
-        <div class="col">
-            <h3>Inicia sesion</h3>
-            <form>
-                <div class="mb-3">
-                    <label  class="form-label">Email address</label>
-                    <input type="text" class="form-control" >
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1">
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+        <hr>
+
+        <div class="row text-center mb-2">
+            <div class="col">
+                <h2>¿Por qué elegirnos?</h2>
+            </div>
         </div>
+        <div class="row text-center">
+            <div class="col-md-6 feature">
+                <img src="img/estadisticas.png" alt="Estadísticas">
+                <h4>Estadísticas</h4>
+                <p>Texto explicativo sobre las estadísticas.</p>
+            </div>
+            <div class="col-md-6 feature">
+                <img src="img/equipo (4).png" alt="Equipo">
+                <h4>Equipo</h4>
+                <p>Texto explicativo sobre el equipo.</p>
+            </div>
+        </div>
+
+    
+
+
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
