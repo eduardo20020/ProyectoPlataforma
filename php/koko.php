b153c6aee0332444f60a0b9d6c965a96e109f1cd
Brandon <brandonde2002@gmail.com>
2024-02-23 22:57:09 -0600

    66666

--- php/koko.php
+++ php/koko.php
@@ -1,7 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:23feb-server.database.windows.net,1433; Database = 23feb-database", "23feb-server-admin", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:23feb-server.database.windows.net,1433; Database = 23feb-database", "23feb-server-admin", "Y0118T1LJHV0RAYB$");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "conexion existosA";
         
