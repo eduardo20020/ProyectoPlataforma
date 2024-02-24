614cfb5ecf4cb45beb14cc67ca35a0a5035af3f5
Brandon <brandonde2002@gmail.com>
2024-02-23 22:50:43 -0600

    Mensaje descriptivo de los cambios realiza5555555

--- php/koko.php
+++ php/koko.php
@@ -1,21 +1,9 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:dos.database.windows.net,1433; Database = dos", "brandon", "12345678X$");
+    $conn = new PDO("sqlsrv:server = tcp:23feb-server.database.windows.net,1433; Database = 23feb-database", "23feb-server-admin", "{your_password_here}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "conexion existosA";
-        // Preparar la consulta SQL
-        $sql = "INSERT INTO prueba (nombre, apellido) VALUES (:nombre, :edad)";
-        $stmt = $conn->prepare($sql);
-        
-        $nombre =44;
-        $apellido = 44;
-        // Asignar valores a los marcadores de posición
-        $stmt->bindParam(':nombre', $nombre);
-        $stmt->bindParam(':edad', $apellido);
-        
-        // Ejecutar la consulta
-        $stmt->execute();
         
 }
 catch (PDOException $e) {
