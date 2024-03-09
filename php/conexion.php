6cd0232a84d6531a3ac9d3f358dd4c0495a780a6
Brandon <brandonde2002@gmail.com>
2024-03-09 08:49:49 -0600

    sirve el todo

--- php/conexion.php
+++ php/conexion.php
@@ -3,8 +3,6 @@
 try {
     $conn = new PDO("sqlsrv:server = tcp:23feb-server.database.windows.net,1433; Database = 23feb-database", "23feb-server-admin", "Y0118T1LJHV0RAYB$");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "conexion existosA";
-    exit();
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
