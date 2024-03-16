75db5759b104fffb1904629669c2815296615a51
Brandon <brandonde2002@gmail.com>
2024-03-15 19:52:07 -0600

    pepererere

--- php/conexion.php
+++ php/conexion.php
@@ -1,11 +1,12 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:23feb-server.database.windows.net,1433; Database = 23feb-database", "23feb-server-admin", "Y0118T1LJHV0RAYB$");
+    $conn = new PDO("sqlsrv:server = tcp:undefined-server.database.windows.net,1433; Database = undefined-database", "undefined-server-admin", "1FF71E83A1PHRCRH$");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
+
 ?>
